0fce8881c9586e2218111438f97ea89c0f1f2bf9
md-rafiul <md.rafiul.pau@gmail.com>
2022-11-26 02:17:01 +0600

    mongodb initialize

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
 //using middleware
 app.use(cors());
 app.use(express.json());
+
+//mongodb
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cdvoxlm.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    const serviceCollection = client
+      .db("serviceReviewDB")
+      .collection("services");
+  } finally {
+  }
+}
+
+run().catch((err) => console.log(err));
 
 //basic
 app.get("/", (req, res) => {
